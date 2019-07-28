2564b1f219031f67a87462045348b5d72f7d2c46
fabianfalon <fabian.falon@gmail.com>
2019-07-28 17:59:51 -0300

    Se agrega apollo y hoc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <React.Fragment>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards />
-  </React.Fragment>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <React.Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </React.Fragment>
+      }
+    </div>
+  )
+}
